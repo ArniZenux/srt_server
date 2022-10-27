2545c814666e3c406528cf3b233eabc3d8015313
Arni Ingi Johannesson <arni.ingi@skaginn3x.com>
2022-10-27 12:46:18 +0000

    ready and all works

--- server.js
+++ server.js
@@ -24,23 +24,26 @@ function catchErrors(fn) {
 async function indexTranslate(req, res) {
   let info  = [ req.body.nafn ];
   
-  console.log(info); 
+  //console.log(info); 
 
   let tyding_setning = ''; 
 
-  if(info === ""){
+  if(info === ''){
     console.log("Engin text í textarea");
   } else {
     let newtext = await translate(info, {from: 'en', to: 'is'});
     tyding_setning += newtext.text; 
   }
-  
-  console.log(tyding_setning); 
-
-  //res.json({ text : text , newtext: tyding_setning});
-  //res.send(`Hello ${req.body.info}`)
-  //res.json(req.body);
+  //console.log(tyding_setning); 
+  res.json(tyding_setning);
 }
+
+/*async function profaIndex(req, res) {
+  let info = [req.body.nafn];
+  console.log(info); 
+  let data = 'Loksins!!';
+  return res.json(data); 
+}*/
 
 /**
  *   GET     
@@ -52,38 +55,32 @@ app.get('/', (req, res) => {
 
 app.get('/translate', (req, res) => {
   res.send("Plz Write some word to Server Translate----");
-  console.log("Plz Write some word to Server Translate - console");
+  //console.log("Plz Write some word to Server Translate - console");
 });
 
-app.get('/profaTranslate', (req, res) => {
-  /*let text = 'profa';
-  let tyding_setning = 'new word for icelandic';
-  res.json({ text : text , newtext: tyding_setning});*/
-  res.send("Hello profa profa - express");
+/*app.get('/profaTranslate', (req, res) => {
+  let data = 'Loksins!!';
   console.log("Hello profa profa - express  -- console");
-});
+  console.log(data); 
+  return res.json(data); 
+});*/
 
 
 /**
  *   POST
  */
 app.post('/translate', catchErrors(indexTranslate));
+//app.post('/profaTranslate2', catchErrors(profaIndex));
 
 /**
  *   Handler error 
  */
 function notFoundHandler(_req, res, _next) { // eslint-disable-line
-    //const title = 'SRT - túlkuþjónusta';
-    //const subtitle = 'Síða fannst ekki';
-    //res.status(404).render('error', { title: title,subtitle : subtitle });
     res.status(404).send('error - 404 - Client finnst ekki');
 }
         
 function errorHandler(err, _req, res, _next) { // eslint-disable-line
     console.error(err);
-    //const title = 'SRT - túlkuþjónusta';
-    //const subtitle = 'Villa kom upp';
-    //res.status(500).render('error', { title: title, subtitle : subtitle });
     res.status(500).send('error - 500 - Villa kom upp');
 }
 
